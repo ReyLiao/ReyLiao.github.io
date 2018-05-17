d3991ab4466e4b567be3ba4f245900a9b27068ba
ReyLiao <804397916@qq.com>
2018-05-18 01:54:24 +0800

    reyliao

---
+++ js/sou/jbtn.js
@@ -0,0 +1,24 @@
+function getHash(){if(location.hash)
+return location.hash.substring(1);return '';}
+if($(window).width()<770){var get_answer=confirm("本站暂不支持手机！\n\n----------\n点击[是]，将跳转到必应手机版。");if(get_answer){var hash=getHash();if(hash)
+window.location.href="https://cn.bing.com/search?setmkt=zh-cn&setlang=zh-cn&q="+hash;else
+window.location.href="https://cn.bing.com/";}}
+var soinput_obj=$('#soinput'),objProgress=$('div.progress>div'),obja=$('#a'),objb=$('#b'),obj_autoSO=$('#autoSO'),autoSO=true;var need_respond=true;var obj_list1_buttons=$("td.set-list-1 button"),obj_list2_buttons=$("td.set-list-2 button");var set_url=[],set_top=[0,0],set_left=[0,0],set_foot=[0,0];respond();if(localStorage["jbtn_autoSO"]===undefined){localStorage["jbtn_autoSO"]=true;obj_autoSO[0].checked=true;autoSO=true;}else{obj_autoSO[0].checked=localStorage["jbtn_autoSO"]==='true';autoSO=obj_autoSO[0].checked;}
+if(localStorage["jbtn_0"]==undefined){set(0,0);obj_list1_buttons.eq(0).removeClass("btn-info").addClass("btn-danger");}else{set(0,localStorage["jbtn_0"]);obj_list1_buttons.eq(localStorage["jbtn_0"]).removeClass("btn-info").addClass("btn-danger");}
+if(localStorage["jbtn_1"]==undefined){set(1,1);obj_list2_buttons.eq(1).removeClass("btn-warning").addClass("btn-success");}else{set(1,localStorage["jbtn_1"]);obj_list2_buttons.eq(localStorage["jbtn_1"]).removeClass("btn-warning").addClass("btn-success");}
+function set(a0b1,set_so){localStorage["jbtn_"+a0b1]=set_so;if(set_so==0){set_url[a0b1]="https://www.baidu.com/s?wd=";set_top[a0b1]=-55;set_left[a0b1]=-100;set_foot[a0b1]=80;}else if(set_so==1){set_url[a0b1]="https://cn.bing.com/search?setmkt=zh-cn&setlang=zh-cn&q=";set_top[a0b1]=-90;set_left[a0b1]=-80;set_foot[a0b1]=80;}else if(set_so==2){set_url[a0b1]="https://www.sogou.com/web?query=";set_top[a0b1]=-55;set_left[a0b1]=-100;set_foot[a0b1]=80;}else if(set_so==3){set_url[a0b1]="https://www.so.com/s?q=";set_top[a0b1]=-60;set_left[a0b1]=-100;set_foot[a0b1]=80;}else{set_url[a0b1]="https://cn.bing.com/search?setmkt=zh-cn&setlang=zh-cn&q=";set_top[a0b1]=-90;set_left[a0b1]=-80;set_foot[a0b1]=80;}
+need_respond=true;}
+obj_list1_buttons.click(function(){obj_list1_buttons.removeClass("btn-danger").addClass("btn-info");$(this).removeClass("btn-info").addClass("btn-danger");});obj_list2_buttons.click(function(){obj_list2_buttons.removeClass("btn-success").addClass("btn-warning");$(this).removeClass("btn-warning").addClass("btn-success");});obj_autoSO.change(function(){localStorage['jbtn_autoSO']=this.checked;autoSO=this.checked;});(function(){if(location.hash){var hash=getHash();hash=decodeURIComponent(hash).replace(/\+/g,' ');soinput_obj[0].value=hash;so();}})();if(localStorage["jbtn_state"]==undefined)
+localStorage["jbtn_state"]=0;function change_state(is_zuo){if(localStorage["jbtn_state"]==0)
+localStorage["jbtn_state"]=is_zuo?-1:1;else
+localStorage["jbtn_state"]=0;respond();if(localStorage["jbtn_state"]==0)
+so();}
+$(window).resizeEnd({delay:50},function(){respond();});function respond(){var winw=$('body').width(),winh=$(window).height(),navh=$("nav.navbar").height(),abx=winw/2;if(localStorage["jbtn_state"]==0){obja.fadeIn('fast').animate({"margin-left":set_left[0],"width":abx-set_left[0]},'fast');objb.fadeIn('fast').animate({"margin-left":abx+set_left[1],"width":abx-set_left[1]},'fast');objProgress[0].style.width='50%';objProgress[1].style.width='50%';}else if(localStorage["jbtn_state"]<0){obja.fadeIn('fast').animate({"margin-left":set_left[0],"width":winw-set_left[0]},'fast');objb.fadeOut('fast');objProgress[0].style.width='75%';objProgress[1].style.width='25%';}else{obja.fadeOut('fast');objb.fadeIn('fast').animate({"margin-left":set_left[1],"width":winw-set_left[0]},'fast');objProgress[0].style.width='25%';objProgress[1].style.width='75%';}
+obja.animate({"margin-top":set_top[0]+navh,"height":winh-set_top[0]-navh+set_foot[0]},'fast');objb.animate({"margin-top":set_top[1]+navh,"height":winh-set_top[1]-navh+set_foot[1]},'fast');need_respond=false;}
+soinput_obj.on('input',function(){if(autoSO)so();});function googleso(){var sowhat_str=soinput_obj[0].value,sowhat=encodeURIComponent(sowhat_str);window.open('https://www.google.com/search?q='+sowhat);}
+function so(){if($.trim(soinput_obj[0].value)!==''){obja[0].src='';objb[0].src='';var sowhat_str=soinput_obj[0].value,sowhat=encodeURIComponent(sowhat_str);if(localStorage["jbtn_state"]==0){obja[0].src=set_url[0]+sowhat;objb[0].src=set_url[1]+sowhat;}else if(localStorage["jbtn_state"]<0)
+obja[0].src=set_url[0]+sowhat;else
+objb[0].src=set_url[1]+sowhat;location.hash=sowhat_str;window.document.title=sowhat_str+'';}else{location.hash='';window.document.title='Sou';}
+if(need_respond)
+respond();objProgress.addClass('progress-bar-striped active');window.setTimeout(function(){objProgress.removeClass('progress-bar-striped active');},2000);}
+so();
